09e78d907bba0625376f53d0fc8681099152adf5
ReDMallard <marco@titori.it>
2023-12-11 17:54:52 +0100

    aggiunge base componente Footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -22,14 +22,105 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Sono un componente 3!</h1>
-    </div>
+    <footer>
+        <div class="container topFoot">
+            <div class="footerCard">
+                <h3>blabla</h3>
+                <ul>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                </ul>
+            </div>
+            <div class="footerCard">
+                <h3>blabla</h3>
+                <ul>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                </ul>
+            </div>
+            <div class="footerCard">
+                <h3>blabla</h3>
+                <ul>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                </ul>
+            </div>
+            <div class="footerCard">
+                <h3>blabla</h3>
+                <ul>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                    <li><a href="#">blabla</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <hr>
+
+        <div class="container lowFoot">
+            <p>blabla</p>
+            <p>i</p>
+
+        </div>
+
+    </footer>
 </template>
 
 <style scoped lang="scss">
-// importo variabili
-// @use './styles/partials/variables' as *;
+footer {
+    height: 22rem;
+    background-color: chocolate;
+}
 
-// ...qui eventuale SCSS di AppComponent
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+}
+
+.topFoot {
+    height: 20rem;
+    background-color: rgba(0, 0, 0, 0.14);
+}
+
+.lowFoot {
+    height: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: rgba(255, 255, 255, 0.14);
+}
+
+.footerCard {
+    height: 15rem;
+    width: 22%;
+
+    >ul {
+        list-style: none;
+        height: 12rem;
+    }
+
+}
+
+a {
+    text-decoration: none;
+    color: grey;
+}
 </style>
