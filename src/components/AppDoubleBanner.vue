aaee6f3c2191ec82e5fc8cbe901a7cec274be1fc
ReDMallard <marco@titori.it>
2023-12-14 12:47:26 +0100

    minor animation

--- src/components/AppDoubleBanner.vue
+++ src/components/AppDoubleBanner.vue
@@ -27,6 +27,7 @@ export default {
         <div class="container">
             <div v-for="elements in catalog">
                 <img class="type1" :src="elements.immagine" alt="">
+                <div class="animation"></div>
                 <h3>{{ elements.titolo }}</h3>
             </div>
         </div>
@@ -54,9 +55,80 @@ export default {
     object-fit: cover;
 }
 
+.animation {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0);
+
+    background: linear-gradient(123deg, #9d9d9d00 0%, rgba(255, 255, 255, 1) 50%, transparent 100%);
+    z-index: 2;
+    width: 100%;
+    height: 18.1rem;
+    opacity: 0;
+    animation: scorri2 1s;
+
+
+    &:hover {
+        animation: scorri 1s;
+    }
+
+}
+
 h3 {
     position: absolute;
     top: 45%;
     right: 2rem;
+}
+
+@keyframes scorri {
+    0% {
+
+        opacity: 0.2;
+        right: -3rem;
+
+
+    }
+
+    90% {
+        opacity: 0.1;
+        transform: translate3d(100%, 0, 0);
+        width: 0;
+
+
+    }
+
+    100% {
+        opacity: 0;
+        left: 0;
+        width: 0;
+
+
+    }
+}
+
+@keyframes scorri2 {
+    0% {
+
+        opacity: 0.4;
+
+
+    }
+
+    90% {
+        opacity: 0.1;
+        transform: translate3d(100%, 0, 0);
+        width: 0;
+
+
+    }
+
+    100% {
+        opacity: 0;
+        left: 0;
+        width: 0;
+
+
+    }
 }
 </style>
